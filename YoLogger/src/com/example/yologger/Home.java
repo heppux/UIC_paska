b9d0fe2eaa9e27a02cfff2789832fb263e835be7
Ville Räsänen <vjrasane@intercal.niksula.hut.fi>
2013-12-22 16:11:59 +0200

    Se toimii?

--- YoLogger/src/com/example/yologger/Home.java
+++ YoLogger/src/com/example/yologger/Home.java
@@ -1,6 +1,10 @@
 package com.example.yologger;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
@@ -9,14 +13,14 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
+import android.support.v4.app.ListFragment;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class Home extends FragmentActivity implements ActionBar.TabListener {
@@ -30,6 +34,7 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
 	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
 	 */
 	SectionsPagerAdapter mSectionsPagerAdapter;
+	List<Fragment> mFragments;
 
 	/**
 	 * The {@link ViewPager} that will host the section contents.
@@ -47,8 +52,13 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
 
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
+		mFragments = new ArrayList<Fragment>();
+		mFragments.add(new DummyListFragment());
+		mFragments.add(new DummyListFragment());
+		mFragments.add(new DummyListFragment());
+		
 		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
+				getSupportFragmentManager(), mFragments);
 
 		// Set up the ViewPager with the sections adapter.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
@@ -76,7 +86,7 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
 					.setTabListener(this));
 		}
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -107,27 +117,24 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
 	 * one of the sections/tabs/pages.
 	 */
 	public class SectionsPagerAdapter extends FragmentPagerAdapter {
+		
+		List<Fragment> fragmentList;
 
-		public SectionsPagerAdapter(FragmentManager fm) {
+		public SectionsPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
 			super(fm);
+			fragmentList = fragments;
 		}
 
 		@Override
 		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
-			Bundle args = new Bundle();
-			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-			fragment.setArguments(args);
-			return fragment;
+			Fragment fragment = fragmentList.get(position);        
+	        return fragment;
 		}
-
+		
 		@Override
 		public int getCount() {
 			// Show 3 total pages.
-			return 3;
+			return fragmentList.size();
 		}
 
 		@Override
@@ -168,8 +175,41 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
 					.findViewById(R.id.section_label);
 			dummyTextView.setText(Integer.toString(getArguments().getInt(
 					ARG_SECTION_NUMBER)));
+//			final ListView listview = (ListView) rootView.findViewById(R.id.activity_list);
+//		    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+//		        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+//		        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+//		        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+//		        "Android", "iPhone", "WindowsMobile" };
+//		    List<String> list = Arrays.asList(values);
+//		    listview.setAdapter(new ArrayAdapter<String>(getActivity(),
+//					android.R.layout.simple_list_item_activated_1, list));
+		    
 			return rootView;
 		}
 	}
+	
+	public static class DummyListFragment extends ListFragment {
 
+		public DummyListFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+		        Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_home_dummy,
+					container, false);
+			final ListView listview = (ListView) rootView.findViewById(android.R.id.list);
+		    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+		        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+		        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+		        "Android", "iPhone", "WindowsMobile" };
+		    List<String> list = Arrays.asList(values);
+		    listview.setAdapter(new ArrayAdapter<String>(getActivity(),
+			android.R.layout.simple_list_item_1, list));
+			return rootView;
+			
+		}
+	}
 }
